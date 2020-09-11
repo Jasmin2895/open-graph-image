a7e0abf2a973faf0878a0a8c57529479761a49c8
buckleader <jasmin28virdi@gmail.com>
2020-09-12 04:10:40 +0530

    get input from the worklfow

--- index.js
+++ index.js
@@ -31,12 +31,9 @@ async function snap(url, file) {
 
     });
     page = await browser.newPage();
-    // let boldText = core.getInput("bold-text");
-    // let plainText = core.getInput("plain-text");
+    let boldText = core.getInput("bold-text");
+    let plainText = core.getInput("plain-text");
 
-    // width: 1024, height: 512 
-    let boldText = "Jasmin Virdi";
-    let plainText = "is a Software Engineer";
     const url = `https://og-image.now.sh/**${boldText}**%20${plainText}?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fhyper-color-logo.svg`;
     const segments = url.split("/");
     const slug = segments[segments.length - 2];
