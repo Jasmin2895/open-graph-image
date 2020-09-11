84e1fd5af75669f2ecb410f59a0e13520588f1bf
buckleader <jasmin28virdi@gmail.com>
2020-09-12 03:38:13 +0530

    set firefox options

--- index.js
+++ index.js
@@ -20,14 +20,13 @@ async function snap(url, file) {
 
 (async () => {
     browser = await puppeteer.launch({
-        headless: false,
-        // product: 'firefox',
-        // extraPrefsFirefox: {
-        //     // Enable additional Firefox logging from its protocol implementation
-        //     // 'remote.log.level': 'Trace',
-        // },
+        product: 'firefox',
+        extraPrefsFirefox: {
+            // Enable additional Firefox logging from its protocol implementation
+            'remote.log.level': 'Trace',
+        },
         // Make browser logs visible
-        // dumpio: true,
+        dumpio: true,
 
     });
     page = await browser.newPage();
