95a4383f71ec3c11f4810c3f16c66c6cb64a8c47
buckleader <jasmin28virdi@gmail.com>
2020-09-13 01:03:51 +0530

    updated files

--- index.js
+++ index.js
@@ -15,13 +15,6 @@ async function snap(url, file) {
                 height: 768
             }
         }
-        // await page.setViewport({
-        //     width: 1024,
-        //     height: 512,
-        //     deviceScaleFactor: 2
-        // });
-        // await page.goto(url);
-        // await page.screenshot({ path: file, type: 'png' });
         await webshot(url, file, options, function (err) {
             // screenshot now saved to google.png
             console.log("screenshot captured")
@@ -34,22 +27,8 @@ async function snap(url, file) {
 
 
 (async () => {
-    // browser = await puppeteer.launch({
-    //     // headless: false,
-    //     product: 'firefox',
-    //     extraPrefsFirefox: {
-    //         // Enable additional Firefox logging from its protocol implementation
-    //         // 'remote.log.level': 'Trace',
-    //     },
-    //     // Make browser logs visible
-    //     dumpio: true,
-
-    // });
-    // page = await browser.newPage();
-    // let boldText = core.getInput("bold-text");
-    // let plainText = core.getInput("plain-text");
-    let boldText = "Jasmin";
-    let plainText = "Virdi";
+    let boldText = core.getInput("bold-text");
+    let plainText = core.getInput("plain-text");
 
     const url = `https://og-image.now.sh/**${boldText}**%20${plainText}?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fhyper-color-logo.svg`;
     const segments = url.split("/");
