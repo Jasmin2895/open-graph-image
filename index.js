648664ab21eff8dc25243333a270939edf4b1597
buckleader <jasmin28virdi@gmail.com>
2020-09-13 02:23:08 +0530

    added theme input

--- index.js
+++ index.js
@@ -27,10 +27,13 @@ async function snap(url, file) {
 
 
 (async () => {
+    let customTheme = "light";
     let boldText = core.getInput("bold-text");
     let plainText = core.getInput("plain-text");
+    if (core.getInput("plain-text"))
+        customTheme = core.getInput("theme");
 
-    const url = `https://og-image.now.sh/**${boldText}**%20${plainText}?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fhyper-color-logo.svg`;
+    const url = `https://og-image.now.sh/**${boldText}**%20${plainText}?theme=${theme}&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fhyper-color-logo.svg`;
     const segments = url.split("/");
     const slug = segments[segments.length - 2];
     let path = `${slug}.png`;
